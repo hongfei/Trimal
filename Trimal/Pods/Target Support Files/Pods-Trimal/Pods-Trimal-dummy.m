2e5ce955255ce84b38084a4f9b23c3d7558b98a1
Hongfei Zhou <mich.zhou1989@gmail.com>
2018-08-27 20:50:12 -0700

    init

---
+++ Trimal/Pods/Target Support Files/Pods-Trimal/Pods-Trimal-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Trimal : NSObject
+@end
+@implementation PodsDummy_Pods_Trimal
+@end
---
+++ Trimal/Trimal/Controllers/TrimalPageViewController.swift
@@ -0,0 +1,13 @@
+//
+//  TrimalPageViewController.swift
+//  Trimal
+//
+//  Created by Hongfei on 2018/8/27.
+//  Copyright © 2018年 Hongfei Zhou. All rights reserved.
+//
+
+import UIKit
+
+class TrimalPageViewController: UIPageViewController {
+    
+}
