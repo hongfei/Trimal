871d96381afc002300b161314129f78e1c32bb22
Hongfei Zhou <mich.zhou1989@gmail.com>
2018-11-21 21:47:18 -0800

    [Fix] Disable deleting when slider expanded

--- Trimal/Trimal/Components/WorldTimeList.swift
+++ Trimal/Trimal/Components/WorldTimeList.swift
@@ -58,7 +58,7 @@ class WorldTimeList: UITableView, UITableViewDataSource, UITableViewDelegate {
     }
 
     func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        return true
+        return indexPath.row != self.expandedCellIndex
     }
 
     func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCell.EditingStyle, forRowAt indexPath: IndexPath) {
